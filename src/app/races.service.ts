710c6fe7113f7868ea44e48e4bd7252c01082bd2
Brian Besaw <bjbesaw@outlook.com>
2018-01-22 21:58:59 -0500

    Mod races .get call.

--- src/app/races.service.ts
+++ src/app/races.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class RacesService {
 
   private racesUrl = 'http://localhost:8080/api/races';
-
+  races = [];
 
   constructor(
     private http: HttpClient,
@@ -16,7 +16,11 @@ export class RacesService {
   // GET races from API
   getRaces () {
     this.http.get(this.racesUrl)
-      .subscribe( data => console.log(data));
+      .subscribe( data => {
+        this.races = data[0];
+        console.log('data');
+        console.log(data[0]);
+      });
   }
 
 }
