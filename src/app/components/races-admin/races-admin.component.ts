afaa1fc56b49ce79722d5e1f551ba82b5c9f90fe
Brian Besaw <bjbesaw@outlook.com>
2018-02-13 13:57:05 -0500

    Added race edit componet.

--- src/app/components/races-admin/races-admin.component.ts
+++ src/app/components/races-admin/races-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RacesService } from '../../services/races.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-races-admin',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RacesAdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private racesService:RacesService,
+    private router: Router) { }
+
+  races = [];
+  currRace = {};
 
   ngOnInit() {
+    this.resetCurrRace();
+    this.getRaces();
+  }
+
+  getRaces() {
+    this.racesService.getRaces()
+      .subscribe(
+        (races: any[]) => {
+          this.races = races;
+          console.log(races);
+        },
+        (err) => console.log(err)
+      );
+  }
+
+  delRace(e) {
+    console.log('delete race');
+    console.log(e.target.parentElement.id);
+  }
+
+  editRace(e) {
+    const selRaceData = this.races.filter( race => race._id === e.target.parentElement.id )[0];
+    this.currRace = Object.assign( {}, this.currRace, {
+      ...selRaceData
+    })
+    console.log('edit race');
+    console.log(e.target.parentElement.id);
+    console.log(this.currRace);
+  }
+
+  onCancel() {
+    this.resetCurrRace();
+  }
+
+  resetCurrRace() {
+    this.currRace = {
+      _id: 'n/a',
+      type: '',
+      city: '',
+      state: '',
+      district: '',
+      candidates: []
+    }
   }
 
 }
