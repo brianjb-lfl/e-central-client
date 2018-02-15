246510e82bd37a9a653ee1479d877ae229461f64
Brian Besaw <bjbesaw@outlook.com>
2018-02-14 23:03:59 -0500

    Race editing logic working, including individual candidate add/del.

--- src/app/components/race-edit/race-edit.component.ts
+++ src/app/components/race-edit/race-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RacesService } from '../../services/races.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
@@ -11,7 +11,7 @@ import { NgForm } from '@angular/forms';
 export class RaceEditComponent implements OnInit {
 
   constructor(
-    private racesService:RacesService,
+    private racesService: RacesService,
     private router: Router) { }
 
   currRace = {
@@ -23,18 +23,54 @@ export class RaceEditComponent implements OnInit {
     candidates: []
   }
 
+  addingCandidate = false;
+  newCandidateName = '';
+
   ngOnInit() {
     this.setCurrRace();
   }
 
+  ngOnDestroy() {
+    this.racesService.editingRace = null;
+  }
+
+  // *** Race editing
+
   onSubmit(form: NgForm) {
     this.racesService.updateRace(this.currRace)
       .subscribe( () => {
-        this.races[currRaceIdx] = Object.assign( {}, this.races[currRaceIdx], {
-          ...this.currRace
-        })
+        this.router.navigate(['/races-admin']);
       })
   }
+
+  onCancelEdit() {
+    this.router.navigate(['/races-admin']);
+  }
+
+  onAddCandidate() {
+    this.addingCandidate = true;
+  }
+
+  onDelCandidate(e) {
+    let tempCandidates = this.currRace.candidates;
+    this.currRace.candidates = tempCandidates.filter( candidate =>
+      candidate.candidate.name !== e.target.parentElement.getElementsByTagName("SPAN")[0].textContent
+    )
+  }
+
+  // *** New candidate editing
+
+  addNewCandidate() {
+    this.currRace.candidates = 
+      [...this.currRace.candidates, {candidate: {name: this.newCandidateName}}];
+    this.clearAddingCandidate();
+  }
+
+  cancelNewCandidate() {
+    this.clearAddingCandidate();
+  }
+
+  // *** Utils
 
   setCurrRace() {
     let tempObj = this.racesService.racesArr[this.racesService.editingRace];
@@ -42,6 +78,9 @@ export class RaceEditComponent implements OnInit {
       this.racesService.racesArr[this.racesService.editingRace] );
   }
 
-
+  clearAddingCandidate() {
+    this.addingCandidate = false;
+    this.newCandidateName = '';
+  }
 
 }
