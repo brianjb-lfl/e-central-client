7163d4da8320a822530cd645fa852a3b9c4b3732
Brian Besaw <bjbesaw@outlook.com>
2018-02-15 03:12:12 -0500

    Voting component set up.

--- src/app/components/races-voting/races-voting.component.ts
+++ src/app/components/races-voting/races-voting.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RacesService } from '../../services/races.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-races-voting',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RacesVotingComponent implements OnInit {
 
-  constructor() { }
+  constructor(  
+    private racesService:RacesService,
+    private router: Router) { }
+
+  races = [];
 
   ngOnInit() {
+    this.getRaces();
+  }
+
+  getRaces() {
+    this.racesService.getRaces()
+      .subscribe(
+        (races: any[]) => {
+          this.racesService.setRaces(races);
+          this.races = this.racesService.racesArr
+          console.log(this.races);
+        },
+        (err) => console.log(err)
+      );
+  }
+
+  onRadioChange(e) {
+    console.log(e);
   }
 
 }
